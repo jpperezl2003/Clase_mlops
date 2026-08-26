cf0a8ff2b977cef31495d68fb6945cfc97cbe95e
jpperezl2003 <jpperezl2003@gmail.com>
2026-08-26 10:25:16 -0600

    Prueba2

--- hello.py
+++ hello.py
@@ -5,3 +5,20 @@ def add(x, y):
 
 if __name__ == "__main__":
     print(add(1, 1))
+
+
+
+"""
+import hashlib
+import random
+
+
+def random_hash():
+    "Generate a random SHA-256 hash"
+    random_value = str(random.random())
+    return hashlib.sha256(random_value.encode()).hexdigest()
+
+
+if __name__ == "__main__":
+    print(random_hash())
+"""
--- test_hello.py
+++ test_hello.py
@@ -3,3 +3,13 @@ from hello import add
 
 def test_add():
     assert (add(1, 1)) == 2
+
+"""
+from hello import random_hash
+    
+def test_random_hash():
+    result = random_hash()
+
+assert isinstance(result, str)
+assert len(result) == 64
+"""
