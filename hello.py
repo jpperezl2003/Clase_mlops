de57ebe8d7622cbcaa716d616b553e3691152cfb
jpperezl2003 <jpperezl2003@gmail.com>
2026-08-26 09:38:02 -0600

    Commit inicial

--- hello.py
+++ hello.py
@@ -1,4 +1,7 @@
-ef add(x, y):
-"""This is an add function"""
-return x + y
-print(add(1, 1))
+def add(x, y):
+    """This is an add function"""
+    return x + y
+
+
+if __name__ == "__main__":
+    print(add(1, 1))
